a0d3490d6ab637c6435aef365a29ef39245778ab
muhammadqodir <dilnavo3@gmail.com>
2022-11-25 16:09:49 +0500

    hack

---
+++ main.js
@@ -0,0 +1,32 @@
+// while(true){console.log("fasdfasdf")}
+
+
+
+
+
+
+
+
+
+
+// let array = [1, "a", 12, "asdf"]
+// let num = []
+// let str = []
+
+// for(let i = 0; i < array.length; i++){
+//     if(typeof array[i] == "number"){
+//         num.push(array[i])
+//     }else{
+//         str.push(array[i])
+//     }
+// }
+
+// console.log(num);
+// console.log(str);
+
+// let son = prompt("son:");
+// let s = 0;
+// for(let i = 0; i <= son; i++){
+//     s += i
+// }
+// console.log(s);
